9722b8bccec8dc3f15e9a893cab695d0336aaea8
TheGiantNinja <yasserfedsi34@gmail.com>
2022-11-16 20:09:42 +0100

    Initial commit

---
+++ funny/app.js
@@ -0,0 +1,8 @@
+var btn = document.querySelector(".btn");
+var position;
+
+btn.addEventListener("mouseover", function() {
+    position ? (position = 0) : (position = 150);
+    btn.style.transform = `translate(${position}px,0px)`;
+    btn.style.transition = "all 0.3s ease"
+})
